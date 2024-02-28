bb44c1bf5f3b7b2268838071668418300e3b5d43
Fatma Kızılkaya <158855561+consguic@users.noreply.github.com>
2024-02-28 13:55:14 +0300

    Create 001_Hello_Mcu.c

---
+++ 001_Hello_Mcu.c
@@ -0,0 +1,11 @@
+/* Yorum ifadeleri*/
+//Dokumantasyon acisindan yorumlar onemlidir.
+
+#include <stdio.h> //On islemci direktifleri. Derleyiciye birtakım direktifler verir.
+
+int main() //Ana program
+//int oldugu icin ana program sonunda bir deger dondurecek
+{
+    printf("Merhaba MCU"); //Ekrana yazdırma fonksiyonu. Hazir fonksiyonlar kullandigimiz gibi kendimiz de fonksiyonlar olusturabiliriz.
+    return 0;
+}
