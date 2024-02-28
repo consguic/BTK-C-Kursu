c871217b919694e9823a362e2b98c3188e118b35
Fatma Kızılkaya <158855561+consguic@users.noreply.github.com>
2024-02-28 17:06:46 +0300

    Create 005_Karakter_Dizileri.c

---
+++ 005_Karakter_Dizileri.c
@@ -0,0 +1,6 @@
+char karakter1 = 'a';
+	char karakters[] = "Fatma";
+	char karakters123[] = {'F', 'a', 't', 'm', 'a', '\0'};
+	printf("%c\n", karakter1 );
+	printf("%lu\n", sizeof(karakters));
+	printf("%lu\n", sizeof(karakters123));
