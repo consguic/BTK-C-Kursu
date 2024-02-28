9e1ccc122bde54e261577329258cc71b7f7e5b7d
Fatma Kızılkaya <158855561+consguic@users.noreply.github.com>
2024-02-28 13:55:47 +0300

    Create 002_Variable.c

---
+++ 002_Variable.c
@@ -0,0 +1,27 @@
+//
+// Created by fatma on 28/02/2024.
+//
+
+#include <stdio.h>
+
+//Birinci degisken tipleri
+int a = 10;
+char name = 'a';
+float puan = 0.2;
+double aggressive_puan= 2.1111;
+void ici_bos_degisken(void);
+
+//Turetilmis degiskenler
+array[4] = {1,2,3,4}; //ayni turden bircok degiskenin bir araya getirildigi statik liste
+//pointer
+//function
+
+//Kullanici tanimlilar
+//struct
+//union
+//enum
+
+int main()
+{
+    return 0;
+}
