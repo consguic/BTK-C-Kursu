1e83dd39d9e17bd201addddceeb23a7f14d165d6
Fatma Kızılkaya <158855561+consguic@users.noreply.github.com>
2024-02-28 15:16:04 +0300

    Update 002_Variable.c
    Hata düzeltildi.

--- 002_Variable.c
+++ 002_Variable.c
@@ -5,14 +5,14 @@
 #include <stdio.h>
 
 //Birinci degisken tipleri
-int a = 10;
+int a = 10, b = 20, c = 30;
 char name = 'a'; //Tek tirnak olmali
 float puan = 0.2;
 double aggressive_puan= 2.1111;
 void ici_bos_degisken(void);
 
 //Turetilmis degiskenler
-array[4] = {1,2,3,4}; //ayni turden bircok degiskenin bir araya getirildigi statik liste
+int array[4] = {1,2,3,4}; //ayni turden bircok degiskenin bir araya getirildigi statik liste
 //pointer
 //function
 
@@ -29,4 +29,5 @@ int main()
 /*Degisken tanimlarken betimleyici ve profesyonel yaklasimlara kullanmak faydali olacaktir.
 g_dToplam >> Global double tipi degisken
 unToplam >> Global olmayan unsigned degisken
+*/
 
